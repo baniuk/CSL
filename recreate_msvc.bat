dff727673f45c0df172820d8d25e57b18107a87a
Baniukiewicz <baniuk@zut.edu.pl>
2014-10-24 10:17:00 +0200

    Fixed bug with data for tests and other copied by file command in case of VC build

--- recreate_msvc.bat
+++ recreate_msvc.bat
@@ -10,6 +10,6 @@ FOR /R %%A IN (*) DO IF %%~nA%%~xA==cmake_install.cmake del /q %%~fA
 FOR /R %%A IN (*) DO IF %%~nA%%~xA==CMakeCache.txt del /q %%~fA
 FOR /D /R %%X IN (CMakeFiles*) DO rmdir "%%X" /s /q
 echo on
-cmake -G "Visual Studio 11 2012" --graphviz=Dep.dot ..\
+cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 11 2012" --graphviz=Dep.dot ..\
 dot -Tpng Dep.dot > Dep.png
 cd ..
