a7c35b9f3ac1bb5be1d20785b60fec1f074fd878
Baniukiewicz <baniuk@zut.edu.pl>
2014-10-24 10:17:38 +0200

    Fixes after merge

--- tests/LV_WeldDetectApprox/LV_WeldDetectApprox_tests.cpp
+++ tests/LV_WeldDetectApprox/LV_WeldDetectApprox_tests.cpp
@@ -179,3 +179,70 @@ TEST(STATIC_WeldDetexApprox,_WeldDetexApprox_2)
 	delete rtg;
 	delete obj;
 }
+
+/**
+* \test STATIC_WeldDetexApprox,_WeldDetexApprox_7
+* \brief Tests main procedre from static lib. Uses WSK sample. Use the following method for example generation:
+* \code {.unparsed}
+* load originals.mat
+* originals{2} = imcrop(originals{2},[2346.5 778.5 3840 1032]); // z f:\Dokumenty\Dysk Google\Praca\Granty\CASELOT
+* savebinarymatrix(originals{2},'testimag2.dat')
+* \endcode
+* \pre Image \c testimag7.dat
+* \post File \c WeldDetectApprox_7.out
+* \see verify.m for result verification
+* \note Uses parameters for WSK dataset
+* \author PB
+* \date 2014/10/24
+*/
+TEST(STATIC_WeldDetexApprox,_WeldDetexApprox_7)
+{
+	C_Matrix_Container *rtg;
+	C_LinearWeld *obj;
+	bool ret;
+	const std::vector<bool> *_lineOK;
+	const std::vector<C_WeldPos> *_weldPos;
+	C_Matrix_Container lineok;
+	C_Matrix_Container weldpos;
+	bool data;
+	C_WeldPos wp;
+
+	C_MatlabExchange dump("WeldDetectApprox_7.out");
+	rtg = new C_Matrix_Container();
+	rtg->ReadBinary("testimag7.dat");
+	obj = new C_LinearWeld(rtg);
+
+	C_LineWeldApprox::setDefaultParams(45000,60,0,0,
+		65535,120,1,70000,
+		0,30,-1,-20000);
+	C_Point cp_x_start(10,0);	// punkt startowy
+	obj->SetProcedureParameters(100,cp_x_start); // inicjalizacja srodowiska, wielkosc bufora 100
+	ret = obj->Start(4,0);	// krok
+	ASSERT_TRUE(ret);
+
+	_lineOK = obj->getLineOK();
+	_weldPos = obj->getweldPos();
+
+	lineok.AllocateData(1,(unsigned int)_lineOK->size());
+	weldpos.AllocateData(6,(unsigned int)_weldPos->size());
+
+	for(unsigned int l=0;l<_lineOK->size();++l)
+	{
+		data = _lineOK->at(l);
+		lineok.SetPixel(0,l,(double)data);
+
+		wp = _weldPos->at(l);
+		weldpos.SetPixel(0,l, wp.D.getX());
+		weldpos.SetPixel(1,l, wp.D.getY());
+		weldpos.SetPixel(2,l, wp.S.getX());
+		weldpos.SetPixel(3,l, wp.S.getY());
+		weldpos.SetPixel(4,l, wp.G.getX());
+		weldpos.SetPixel(5,l, wp.G.getY());
+	}
+
+	dump.AddEntry2D<double>(lineok.data, lineok._rows, lineok._cols, "lineok");
+	dump.AddEntry2D<double>(weldpos.data, weldpos._rows, weldpos._cols, "weldpos");
+
+	delete rtg;
+	delete obj;
+}
