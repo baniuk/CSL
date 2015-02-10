eea6a46ad43bd31443b1d65ef0189aa4883f3ccb
Baniukiewicz <baniuk@zut.edu.pl>
2015-02-10 11:08:11 +0100

    Added M_WeldDetectApprox project (tested)

--- src/LV_WeldDetectApprox/M_WeldDetectApprox_DLL_Wrapper.cpp
+++ src/LV_WeldDetectApprox/M_WeldDetectApprox_DLL_Wrapper.cpp
@@ -25,11 +25,11 @@ void mexFunction(int nlhs,
 	const mxArray *prhs[])
 {
 	if(nrhs!=1)
-        mexErrMsgIdAndTxt("M_WeldDetectApprox_DLL_Wrapper","One input required.");
+        mexErrMsgIdAndTxt("M_WeldDetectApprox:IOError","One input required.");
     if(nlhs!=1)
-        mexErrMsgIdAndTxt("M_WeldDetectApprox_DLL_Wrapper","One output required.");
+        mexErrMsgIdAndTxt("M_WeldDetectApprox:IOError","One output required.");
 	if (mxGetClassID(prhs[0]) != mxDOUBLE_CLASS)
-    	mexErrMsgIdAndTxt("M_WeldDetectApprox_DLL_Wrapper","Input must be DOUBLE class.");
+    	mexErrMsgIdAndTxt("M_WeldDetectApprox:IOError","Input must be DOUBLE class scalled to UINT range.");
 
 	size_t a;
 	size_t rows = mxGetM(prhs[0]);
@@ -47,7 +47,8 @@ void mexFunction(int nlhs,
 			input_image[r*cols + c] = m_input[a++];
 
 	// debug - copy in->out
-	memcpy(output_image, input_image, cols*rows);
+	for (size_t i = 0; i < rows*cols; i++)
+		output_image[i] = input_image[i];
 
  	double A, E;
  	Matrix_Container rtg;	// temporary object for compatibility
@@ -62,29 +63,29 @@ void mexFunction(int nlhs,
 	rtg.data = input_image;
 	rtg._cols = cols;
 	rtg._rows = rows;
-// 
-//  	std::unique_ptr<C_LinearWeld> obj(new C_LinearWeld(&rtg)); // main object
-//  	C_Point cp_x_start(static_cast<double>(start_x_point), 0.0);	// punkt startowy
-//  	ParamEstimation<double>(input_image, cols, rows, start_x_point, A, E); // estimation of initial params
-//  	C_LineWeldApprox::setDefaultParams(static_cast<double>(A), 60, 0,
-//  		static_cast<double>(E),
-//  		65535,340,1,70000,
-//  		0,10,-1,-20000); // setting up initial params
-//  	obj->SetProcedureParameters(buffor_size,cp_x_start); // initialize starting point and buffor size
-//  	if (obj->Start(step, 0, weld_edge) == false)
-// 		mexErrMsgIdAndTxt("M_WeldDetectApprox_DLL_Wrapper", "Error in Start().");
-//  
-//  	_lineOK = obj->getLineOK();
-//  	_weldPos = obj->getweldPos();
-//  
-//  	cv::Mat cvimage(rtg._rows, rtg._cols, CV_16U, output_image);// rozmiar
-//  	cvimage = cv::Scalar(0);
-//  	fillWeldShape(_weldPos, _lineOK, cvimage);
+ 
+  	std::unique_ptr<C_LinearWeld> obj(new C_LinearWeld(&rtg)); // main object
+  	C_Point cp_x_start(static_cast<double>(start_x_point), 0.0);	// punkt startowy
+  	ParamEstimation<double>(input_image, cols, rows, start_x_point, A, E); // estimation of initial params
+  	C_LineWeldApprox::setDefaultParams(static_cast<double>(A), 60, 0,
+  		static_cast<double>(E),
+  		65535,340,1,70000,
+  		0,10,-1,-20000); // setting up initial params
+  	obj->SetProcedureParameters(buffor_size,cp_x_start); // initialize starting point and buffor size
+  	if (obj->Start(step, 0, weld_edge) == false)
+ 		mexErrMsgIdAndTxt("M_WeldDetectApprox:Start", "Error in Start().");
+  
+  	_lineOK = obj->getLineOK();
+  	_weldPos = obj->getweldPos();
+  
+  	cv::Mat cvimage(rtg._rows, rtg._cols, CV_16U, output_image);// rozmiar
+  	cvimage = cv::Scalar(0);
+  	fillWeldShape(_weldPos, _lineOK, cvimage);
  
  	plhs[0] = mxCreateDoubleMatrix(rtg._rows,rtg._cols,mxREAL);
  	double*	p_out = mxGetPr(plhs[0]);
-	a = 0;
-	for (size_t c = 0; c < cols; c++)
+	// copy from local array to matlab array columns ordred)
+	for (size_t c = 0, a = 0; c < cols; c++)
 		for (size_t r = 0; r < rows; r++)
 			p_out[a++] = output_image[r*cols + c];
 			
